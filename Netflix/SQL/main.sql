ef5577ebfef1224bee84b4a5699f58c6baaa1198
Oli Dare <olidare@Olis-MacBook-Pro.local>
2025-05-14 19:00:09 -0500

    adding SQL and Jupyter notepad of Netflix analysis

---
+++ Netflix/SQL/main.sql
@@ -0,0 +1,16 @@
+--remove duplicates 
+select show_id, COUNT(*) 
+from netflix_titles
+group by show_id 
+having COUNT(*)>1
+
+-- 
+SELECT n.* 
+FROM netflix_titles n
+JOIN (
+    SELECT UPPER(title) AS utitle, type
+    FROM netflix_titles
+    GROUP BY UPPER(title), type
+    HAVING COUNT(*) > 1
+) dup ON UPPER(n.title) = dup.utitle AND n.type = dup.type
+ORDER BY n.title;
