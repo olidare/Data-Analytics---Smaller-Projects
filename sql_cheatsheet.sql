eff2868e9c94351d4309eb39a770e21959e1093c
Oli Dare <odare3@gmail.com>
2025-05-27 12:57:46 -0500

    Update sql_cheatsheet.sql

--- sql_cheatsheet.sql
+++ sql_cheatsheet.sql
@@ -304,8 +304,37 @@ SELECT * FROM RecursiveMonths;
 
 ----
 
-HARDER EXAMPLE:
+HARDER EXAMPLE Order a Company by hierarchy - Director at the Top:
 
+  WITH RECURSIVE company_hierarchy AS (
+  SELECT    id,
+            first_name,
+            last_name,
+            boss_id,
+        0 AS hierarchy_level
+  FROM employees
+  WHERE boss_id IS NULL
+  
+  UNION ALL
+  
+    SELECT  
+    		e.id,
+            e.first_name,
+            e.last_name,
+            e.boss_id,
+            hierarchy_level+1
+  FROM employees e, company_hierarchy ch
+  WHERE e.boss_id = ch.id
+ )
+ 
+SELECT ch.first_name AS employee_first_name,
+       ch.last_name AS employee_last_name,
+       CONCAT(e.first_name, " ", e.last_name)  AS boss_name,
+       hierarchy_level
+FROM company_hierarchy ch
+LEFT JOIN employees e
+ON ch.boss_id = e.id
+ORDER BY ch.hierarchy_level, ch.boss_id;
 
 
   
